923379f4a67bc152a54544f8cf45b76440d8263c
Drew Haven <drew.haven@gmail.com>
2022-12-01 12:23:29 -0800

    Day 01: Very simple optimization

--- src/Day01.hs
+++ src/Day01.hs
@@ -2,6 +2,7 @@ module Day01 where
 
 import Data.Attoparsec.Text (endOfLine, sepBy, decimal, Parser)
 import Data.Foldable (maximum)
+import Data.List qualified as List
 
 import Exercise ( Exercise(..), Solution(..) )
 
@@ -24,5 +25,16 @@ parser =
 part1 :: Input -> Int
 part1 = maximum . map sum
 
+part2Simple :: Input -> Int
+part2Simple = sum . take 3 . reverse . sort . map sum
+
 part2 :: Input -> Int
-part2 = sum . take 3 . reverse . sort . map sum
+part2 = sum . findNLargest 3 . map sum
+  where
+    findNLargest n list = findNLargest' n [] list
+
+    findNLargest' _ found [] = found
+    findNLargest' n found (l:ls)
+      | length found < n = findNLargest' n (l : found) ls
+      | l > List.head found = findNLargest' n (List.insert l $ take (n - 1) found) ls
+      | otherwise = findNLargest' n found ls
