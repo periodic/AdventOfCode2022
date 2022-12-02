bb782b7655d3e871eda2ce42cd1d6cec7f808b9d
Drew Haven <drew.haven@gmail.com>
2022-12-02 11:40:14 -0800

    Day 02: Formatting

--- src/Day02.hs
+++ src/Day02.hs
@@ -1,8 +1,10 @@
 module Day02 where
 
-import Exercise
-    ( Exercise(..), Solution(..) )
-import Data.Attoparsec.Text (char, Parser, skipSpace, endOfLine, sepBy, endOfInput)
+import Data.Attoparsec.Text (Parser, char, endOfInput, endOfLine, sepBy, skipSpace)
+import Exercise (
+  Exercise (..),
+  Solution (..),
+ )
 
 exercise :: Exercise
 exercise =
@@ -35,20 +37,21 @@ type Input = [(Throw, UnknownCol)]
 parser :: Parser Input
 parser =
   (round `sepBy` endOfLine) <* skipSpace <* endOfInput
-  where
-    round = (,) <$> (theirThrow <* skipSpace) <*> ourThrow
+ where
+  round = (,) <$> (theirThrow <* skipSpace) <*> ourThrow
 
-    theirThrow =
-      (char 'A' $> Rock)
+  theirThrow =
+    (char 'A' $> Rock)
       <|> (char 'B' $> Paper)
       <|> (char 'C' $> Scissors)
 
-    ourThrow =
-      (char 'X' $> X)
-      <|>(char 'Y' $> Y)
-      <|>(char 'Z' $> Z)
+  ourThrow =
+    (char 'X' $> X)
+      <|> (char 'Y' $> Y)
+      <|> (char 'Z' $> Z)
 
-data Result = Player1Won | Draw | Player2Won deriving (Eq)
+data Result = Player1Won | Draw | Player2Won
+  deriving (Eq)
 
 roundResult :: Throw -> Throw -> Result
 roundResult them us
@@ -59,25 +62,25 @@ roundResult them us
 score :: Throw -> Throw -> Int
 score them us =
   let shapeScore = case us of
-          Rock -> 1
-          Paper -> 2
-          Scissors -> 3
+        Rock -> 1
+        Paper -> 2
+        Scissors -> 3
       resultScore = case roundResult them us of
-          Player1Won -> 0
-          Draw -> 3
-          Player2Won -> 6
-  in shapeScore + resultScore
+        Player1Won -> 0
+        Draw -> 3
+        Player2Won -> 6
+   in shapeScore + resultScore
 
 part1 :: Input -> Int
 part1 = sum . map (uncurry score . fmap unknownToThrow)
-  where
-    unknownToThrow X = Rock
-    unknownToThrow Y = Paper
-    unknownToThrow Z = Scissors
+ where
+  unknownToThrow X = Rock
+  unknownToThrow Y = Paper
+  unknownToThrow Z = Scissors
 
 part2 :: Input -> Int
 part2 = sum . map (uncurry score . makeChoice)
-  where
-    makeChoice (them, X) = (them, worse them)
-    makeChoice (them, Z) = (them, better them)
-    makeChoice (them, Y) = (them, them)
+ where
+  makeChoice (them, X) = (them, worse them)
+  makeChoice (them, Z) = (them, better them)
+  makeChoice (them, Y) = (them, them)
